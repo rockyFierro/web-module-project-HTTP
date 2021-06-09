8769506e8738eac5cf33a556a885c8fd2089f222
rockyFierro <finnslighthouse@gmail.com>
2021-06-09 11:21:56 -0700

    useParams used to gain access to the :id paramater of the url in EditMovieForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React, {useEffect, useState} from "react";
-
+//routing
 import {Redirect, Route, Switch} from "react-router-dom";
+//3rd party
+import axios from 'axios';
+//components
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
-
 import MovieHeader from './components/MovieHeader';
-
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
-
-import axios from 'axios';
-
+//App component
 const App = (props) => {
     const [movies, setMovies] = useState([]);
     const [favoriteMovies, setFavoriteMovies] = useState([]);
-
+//initial call out
     useEffect(() => {
         axios.get('http://localhost:5000/api/movies')
             .then(res => {
@@ -24,14 +23,15 @@ const App = (props) => {
                 console.log(err);
             });
     }, []);
-
+//handlers
+ //delete
     const deleteMovie = (id) => {
     }
-
+ //add
     const addToFavorites = (movie) => {
 
     }
-
+//jsx - return component
     return (
         <div>
             <nav className="navbar navbar-dark bg-dark">
@@ -53,7 +53,7 @@ const App = (props) => {
                             <Movie/>
                         </Route>
 
-                        <Route path="/mvies">
+                        <Route path="/movies">
                             <MovieList movies={movies}/>
                         </Route>
 
