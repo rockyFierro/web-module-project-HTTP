d1b5418b433dfc74c74c598dde32ae04b0a757fb
rockyFierro <finnslighthouse@gmail.com>
2021-06-08 21:55:52 -0700

    EditMovieForm added to app>switch>route>/edit/:id

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
@@ -12,57 +12,59 @@ import FavoriteMovieList from './components/FavoriteMovieList';
 import axios from 'axios';
 
 const App = (props) => {
-  const [movies, setMovies] = useState([]);
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/movies')
-      .then(res => {
-        setMovies(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/movies')
+            .then(res => {
+                setMovies(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
 
-  const deleteMovie = (id)=> {
-  }
+    const deleteMovie = (id) => {
+    }
 
-  const addToFavorites = (movie) => {
-    
-  }
+    const addToFavorites = (movie) => {
 
-  return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
-      </nav>
+    }
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
-          <Switch>
-            <Route path="/movies/edit/:id">
-            </Route>
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-dark">
+                <span className="navbar-brand"><img width="40px" alt=""
+                                                    src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
+            </nav>
 
-            <Route path="/movies/:id">
-              <Movie/>
-            </Route>
+            <div className="container">
+                <MovieHeader/>
+                <div className="row ">
+                    <FavoriteMovieList favoriteMovies={favoriteMovies}/>
 
-            <Route path="/movies">
-              <MovieList movies={movies}/>
-            </Route>
+                    <Switch>
+                        <Route path="/movies/edit/:id">
+                            <EditMovieForm/>
+                        </Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
-            </Route>
-          </Switch>
+                        <Route path="/movies/:id">
+                            <Movie/>
+                        </Route>
+
+                        <Route path="/movies">
+                            <MovieList movies={movies}/>
+                        </Route>
+
+                        <Route path="/">
+                            <Redirect to="/movies"/>
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 
